ac5b6a8f1f8aa44adf508d06ce60c2cf6bb3df22
Maciej Szczytowski <maciej.szczytowski@mobeelizer.com>
2012-08-03 11:16:18 +0200

    change default length of text field to 255

--- src/main/java/com/mobeelizer/mobile/android/types/TextFieldTypeHelper.java
+++ src/main/java/com/mobeelizer/mobile/android/types/TextFieldTypeHelper.java
@@ -33,69 +33,69 @@ import com.mobeelizer.java.model.MobeelizerFieldAccessor;
 
 public class TextFieldTypeHelper extends FieldTypeHelper {
 
-    public TextFieldTypeHelper() {
-        super(MobeelizerFieldType.TEXT);
-    }
+	public TextFieldTypeHelper() {
+		super(MobeelizerFieldType.TEXT);
+	}
 
-    @Override
-    public String[] getTypeDefinition(final MobeelizerFieldAccessor field, final boolean required, final Object defaultValue,
-            final Map<String, String> options) {
-        return new String[] { getSingleDefinition(field.getName(), "TEXT(" + getMaxLength(options) + ")", required,
-                (String) defaultValue, true) };
-    }
+	@Override
+	public String[] getTypeDefinition(final MobeelizerFieldAccessor field, final boolean required, final Object defaultValue,
+			final Map<String, String> options) {
+		return new String[] { getSingleDefinition(field.getName(), "TEXT(" + getMaxLength(options) + ")", required,
+				(String) defaultValue, true) };
+	}
 
-    @Override
-    protected <T> void setNotNullValueFromDatabaseToEntity(final Cursor cursor, final int columnIndex, final T entity,
-            final MobeelizerFieldAccessor field, final Map<String, String> options) {
-        setValue(field, entity, getType().convertFromDatabaseValueToEntityValue(field, cursor.getString(columnIndex)));
-    }
+	@Override
+	protected <T> void setNotNullValueFromDatabaseToEntity(final Cursor cursor, final int columnIndex, final T entity,
+			final MobeelizerFieldAccessor field, final Map<String, String> options) {
+		setValue(field, entity, getType().convertFromDatabaseValueToEntityValue(field, cursor.getString(columnIndex)));
+	}
 
-    @Override
-    protected void setNotNullValueFromEntityToDatabase(final ContentValues values, final Object value,
-            final MobeelizerFieldAccessor field, final Map<String, String> options,
-            final MobeelizerErrorsBuilder errors) {
-        String stringValue = (String) getType().convertFromEntityValueToDatabaseValue(field, value, options, errors);
+	@Override
+	protected void setNotNullValueFromEntityToDatabase(final ContentValues values, final Object value,
+			final MobeelizerFieldAccessor field, final Map<String, String> options,
+			final MobeelizerErrorsBuilder errors) {
+		String stringValue = (String) getType().convertFromEntityValueToDatabaseValue(field, value, options, errors);
 
-        if (!errors.hasNoErrors()) {
-            return;
-        }
+		if (!errors.hasNoErrors()) {
+			return;
+		}
 
-        values.put(field.getName(), stringValue);
-    }
+		values.put(field.getName(), stringValue);
+	}
 
-    @Override
-    protected void setNullValueFromEntityToDatabase(final ContentValues values, final MobeelizerFieldAccessor field,
-            final Map<String, String> options, final MobeelizerErrorsBuilder errors) {
-        values.put(field.getName(), (String) null);
-    }
+	@Override
+	protected void setNullValueFromEntityToDatabase(final ContentValues values, final MobeelizerFieldAccessor field,
+			final Map<String, String> options, final MobeelizerErrorsBuilder errors) {
+		values.put(field.getName(), (String) null);
+	}
 
-    @Override
-    protected void setNotNullValueFromDatabaseToMap(final Cursor cursor, final int columnIndex, final Map<String, String> values,
-            final MobeelizerFieldAccessor field, final Map<String, String> options) {
-        values.put(field.getName(), cursor.getString(columnIndex));
-    }
+	@Override
+	protected void setNotNullValueFromDatabaseToMap(final Cursor cursor, final int columnIndex, final Map<String, String> values,
+			final MobeelizerFieldAccessor field, final Map<String, String> options) {
+		values.put(field.getName(), cursor.getString(columnIndex));
+	}
 
-    @Override
-    protected void setNullValueFromDatabaseToMap(final Cursor cursor, final int columnIndex, final Map<String, String> values,
-            final MobeelizerFieldAccessor field, final Map<String, String> options) {
-        values.put(field.getName(), null);
-    }
+	@Override
+	protected void setNullValueFromDatabaseToMap(final Cursor cursor, final int columnIndex, final Map<String, String> values,
+			final MobeelizerFieldAccessor field, final Map<String, String> options) {
+		values.put(field.getName(), null);
+	}
 
-    @Override
-    protected void setNotNullValueFromMapToDatabase(final ContentValues values, final String value,
-            final MobeelizerFieldAccessor field, final Map<String, String> options,
-            final MobeelizerErrorsBuilder errors) {
-        values.put(field.getName(), value);
-    }
+	@Override
+	protected void setNotNullValueFromMapToDatabase(final ContentValues values, final String value,
+			final MobeelizerFieldAccessor field, final Map<String, String> options,
+			final MobeelizerErrorsBuilder errors) {
+		values.put(field.getName(), value);
+	}
 
-    @Override
-    protected void setNullValueFromMapToDatabase(final ContentValues values, final MobeelizerFieldAccessor field,
-            final Map<String, String> options, final MobeelizerErrorsBuilder errors) {
-        values.put(field.getName(), (String) null);
-    }
+	@Override
+	protected void setNullValueFromMapToDatabase(final ContentValues values, final MobeelizerFieldAccessor field,
+			final Map<String, String> options, final MobeelizerErrorsBuilder errors) {
+		values.put(field.getName(), (String) null);
+	}
 
-    private int getMaxLength(final Map<String, String> options) {
-        return options.containsKey("maxLength") ? Integer.valueOf(options.get("maxLength")) : 4096;
-    }
+	private int getMaxLength(final Map<String, String> options) {
+		return options.containsKey("maxLength") ? Integer.valueOf(options.get("maxLength")) : 255;
+	}
 
 }
